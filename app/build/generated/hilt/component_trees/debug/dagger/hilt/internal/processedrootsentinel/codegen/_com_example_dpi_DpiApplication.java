16cc73a804ba97f201cce0e092060b8a63d482bb
PrashamJ17 <jainprasham05@gmail.com>
2026-02-14 13:03:10 +0530

    Remove build artifacts and add .gitignore

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_example_dpi_DpiApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.example.dpi.DpiApplication"
-)
-public final class _com_example_dpi_DpiApplication {
-}
